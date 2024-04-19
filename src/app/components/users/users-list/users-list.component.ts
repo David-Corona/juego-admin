9c7ea645e7d65d71901c645e13e6642704e16a26
David Corona <corona_121@hotmail.com>
2024-04-20 00:35:51 +0200

    Created Users-List

--- src/app/components/users/users-list/users-list.component.ts
+++ src/app/components/users/users-list/users-list.component.ts
@@ -1,22 +1,27 @@
 import { Component } from '@angular/core';
 
+import { MessageService } from 'primeng/api';
+
 import { UsersService } from '../users.service';
 
 
+
 @Component({
-//   selector: 'app-users-list',
-//   standalone: true,
-//   imports: [],
-  templateUrl: './users-list.component.html',
-  styleUrl: './users-list.component.scss'
+    //   selector: 'app-users-list',
+    //   standalone: true,
+    //   imports: [],
+    templateUrl: './users-list.component.html',
+    //   styleUrl: './users-list.component.scss'
+    providers: [MessageService]
 })
 export class UsersListComponent {
 
     users: any; // TODO - User type
-
+    cols: any[] = [];
 
     constructor(
         private usersService: UsersService,
+        private messageService: MessageService
         // protected router: Router,
 
       ) { }
@@ -24,11 +29,19 @@ export class UsersListComponent {
 
     ngOnInit(): void {
 
+        this.cols = [
+            { field: 'id', header: 'ID' },
+            { field: 'nombre', header: 'Nombre' },
+            { field: 'email', header: 'Email' },
+            { field: 'role', header: 'Rol' }
+        ];
+
         // TODO - Resolver
         this.usersService.getUsuarios()
         .subscribe({
           next: resp => {
-            this.users = resp;
+            this.users = resp.data;
+            this.messageService.add({ severity: 'success', summary: '¡Éxito!', detail: 'Usuarios cargados correctamente'});
             console.log(resp);
           },
           error: e => {
--- src/app/components/users/users-list/users-list.module.ts
+++ src/app/components/users/users-list/users-list.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { ToastModule } from 'primeng/toast';
+import { TableModule } from 'primeng/table';
+
+import { UsersListComponent } from './users-list.component';
 import { UsersListRoutingModule } from './users-list-routing.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [UsersListComponent],
   imports: [
     CommonModule,
-    UsersListRoutingModule
+    UsersListRoutingModule,
+    TableModule,
+    ToastModule,
+
   ]
 })
 export class UsersListModule { }
